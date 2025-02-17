68f8f5dfa59401e84f640f83412e10e7c92f7092
Salvaji Bhuvan <21e51a6747@hitam.org>
2025-02-17 19:18:34 +0000

    started javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert("Hello world!")
+
+console.log("Code is running")
+// let a = prompt("enter a number : ")
+// var isTrue = confirm("are u sure to quit!")
+
+// if(isTrue){
+//     console.log("are just quitted!")
+// }
+// else{
+//     console.log("oh wow u didn't quit yet! nice to have u backk")
+// }
+let a = 10;
+console.log(a)
